8559e97efcd03a9d2f05abf783a641c00c5a690e
Your Name <matthew.fong@gmail.com>
2011-11-22 00:01:58 -0500

    Added vote counting

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,31 @@
+class VotesController < ApplicationController
+  def index
+  end
+
+  def new
+    @vote = Vote.new
+    
+    respond_to do |format|
+      format.html
+      formal.xml
+    end
+  end
+
+  def create
+    @vote = Vote.new(params[:vote])
+    post = params[:vote][:post_id]
+    uid = params[:vote][:user_id] 
+    if @vote.save
+      flash[:success] = "You voted for the article"
+      redirect_to root_path
+    else
+      flash[:failure] = "You did not vote"
+      render 'posts/index'
+    end
+      
+  end
+
+  def destroy
+  end
+
+end
