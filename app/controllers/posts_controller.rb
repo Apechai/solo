4241f6a16b31c46e336387a7f85e7e8a733e261f
Your Name <matthew.fong@gmail.com>
2011-12-06 00:13:20 -0500

    only new post requires authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	before_filter :authenticate_user!
+before_filter :authenticate_user!, :only => [:new, :create]
 	
   def index
     @vote = Vote.new(params[:vote])
